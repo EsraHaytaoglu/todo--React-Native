d3f123c975875ea24f6374c2a50c07007cbefb9c
EsraHaytaoglu <esracebi.81@gmail.com>
2022-11-17 17:41:09 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  Dimensions,
+} from 'react-native';
+
+const INITIAL_STATE = [
+  {id: 1, baslik: 'Learn React Native', tamamlandi: false},
+  {id: 2, baslik: 'Learn React', tamamlandi: true},
+];
+
+function App() {
+  const [liste, setListe] = useState(INITIAL_STATE); // todo elemanları
+  const [yeniBaslik, setYeniBaslik] = useState('');
+  const [yapilacak, setYapilacak] = useState(1);
+
+  useEffect(() => {
+    const activeliste = liste.filter(el => el.tamamlandi !== true);
+    setYapilacak(activeliste.length);
+    console.log(activeliste);
+  }, [liste]);
+
+  const click = id => {
+    setListe(
+      liste.map(el =>
+        el.id === id ? {...el, tamamlandi: !el.tamamlandi} : el,
+      ),
+    );
+  };
+
+  return (
+    <View styles={styles.outerContainer}>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Yapılacaklar</Text>
+        <Text style={styles.number}>{yapilacak}</Text>
+      </View>
+      <View style={styles.body_container}>
+        <View>
+          {liste.map(item => (
+            <Text
+              style={item.tamamlandi ? styles.completed : styles.active}
+              key={item.id}
+              onPress={() => click(item.id)}
+              onLongPress={e => {
+                setListe(liste.filter(el => el.id !== item.id));
+              }}>
+              {item.baslik}
+            </Text>
+          ))}
+        </View>
+        <View style={styles.new_todo_container}>
+          <View style={styles.todo_input}>
+            <TextInput
+              value={yeniBaslik}
+              onChangeText={setYeniBaslik}
+              placeholder="Yapılacak..."
+              style={styles.placeholder}
+            />
+          </View>
+          <Button
+            disabled={yeniBaslik ? false : true}
+            color={yeniBaslik && 'orange'}
+            title="Kaydet"
+            onPress={e => {
+              e.preventDefault();
+              setListe([
+                ...liste,
+                {id: Date.now(), baslik: yeniBaslik, tamamlandi: false},
+              ]);
+              setYeniBaslik('');
+              setYapilacak(yapilacak + 1);
+            }}
+            style={styles.active_button}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  container: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  body_container: {
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: 'orange',
+    margin: 10,
+    flex: 1,
+  },
+  number: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'orange',
+    margin: 10,
+  },
+  new_todo_container: {
+    backgroundColor: '#708090',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  placeholder: {
+    fontSize: 18,
+  },
+  disable_button: {
+    overflow: 'hidden',
+    borderRadius: 15,
+  },
+  active_button: {
+    backgroundColor: 'orange',
+  },
+  todo_input: {
+    borderBottomColor: '#f0f8ff',
+    borderBottomWidth: 1,
+    margin: 10,
+  },
+  active: {
+    backgroundColor: '#3cb371',
+    height: Dimensions.get('window').height / 14,
+    borderRadius: 10,
+    margin: 10,
+    fontSize: 20,
+    color: 'white',
+    padding: 10,
+  },
+  completed: {
+    backgroundColor: '#696969',
+    color: '#a9a9a9',
+    margin: 10,
+    borderRadius: 10,
+    fontSize: 20,
+    padding: 10,
+    height: Dimensions.get('window').height / 14,
+    textDecorationLine: 'line-through',
+  },
+});
+export default App;
